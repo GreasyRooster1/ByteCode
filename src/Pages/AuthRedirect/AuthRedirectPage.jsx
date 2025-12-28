497243005ddcf69cb3d23336551d0d6494a2a0c4
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-28 13:21:20 -0800

    better auth redirect flow

--- src/Pages/AuthRedirect/AuthRedirectPage.jsx
+++ src/Pages/AuthRedirect/AuthRedirectPage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ScreenPage from "~components/screenPage/ScreenPage.jsx";
 import {Navigate} from "react-router-dom";
+import {AuthProvider, useAuth} from "react-oidc-context";
+import LoadingScreen from "~components/LoadingScreen.jsx";
+import Error from "~components/Error.jsx";
 
 function AuthRedirectPage() {
-    return (
-        <>
-        <ScreenPage>
-            Logging in...
+    const auth = useAuth();
+
+    if (auth.isLoading) {
+        return <LoadingScreen />;
+    }
+
+    if (auth.error) {
+        //todo error screen
+        return <Error message={"Failed to login!"}/>
+    }
+
+    if(auth.isAuthenticated) {
+        return (
             <Navigate to="/" replace />
-        </ScreenPage>
-        </>
-    );
+        );
+    }else{
+        return auth.signinRedirect();
+    }
+
 }
 
 export default AuthRedirectPage;
