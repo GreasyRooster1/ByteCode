8560b91f2af9598eb92df6ae130b168fd34f8105
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-29 17:05:45 -0800

    showing and hiding

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Section} from "@radix-ui/themes";
 import styles from "./nav.module.css"
 
 function Nav(props) {
+    const [scrollPosition, setScrollPosition] = useState(0)
+
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
+
+    let isVisible = scrollPosition < 10;
+
     return (
-        <div className={`${styles.nav} ${styles.hidden}`} >
+        <div className={`${styles.nav} ${isVisible?styles.hidden:""}`} >
             <div className={`${styles.navInner} ${props.className}`}>
                 {props.children}
             </div>
