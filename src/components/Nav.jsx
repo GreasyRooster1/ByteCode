d50dfbe39fe3bfca75ceec499e0e8130a6d28a35
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-29 17:02:48 -0800

    hidden nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,9 @@ import styles from "./nav.module.css"
 
 function Nav(props) {
     return (
-        <div className={`${styles.nav}`} >
+        <div className={`${styles.nav} ${styles.hidden}`} >
             <div className={`${styles.navInner} ${props.className}`}>
-            {props.children}
+                {props.children}
             </div>
         </div>
     );
