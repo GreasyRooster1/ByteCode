5de76d6c6c6e115adfee363f989b0c0c05d7ec80
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-29 16:35:39 -0800

    reorder styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
 import "@radix-ui/themes/styles.css";
 import "./radixTheme.css"
+import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom";
 import {Theme} from "@radix-ui/themes";
