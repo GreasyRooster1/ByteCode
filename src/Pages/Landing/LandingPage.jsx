1c63872496824b99ebea06d87dda9d34783aedc8
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-30 15:52:20 -0800

    more info gradient

--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -53,7 +53,7 @@ function LandingPage() {
                 </Flex>
             </LandingSection>
 
-            <LandingSection height="10vh" justify="center" align="center" direction="column">
+            <LandingSection height="20vh" justify="center" align="center" direction="column" className={styles.moreInfo}>
                 <Text size="7" weight="bold">More Info</Text>
                 <Text size="5" weight="bold">V</Text>
             </LandingSection>
