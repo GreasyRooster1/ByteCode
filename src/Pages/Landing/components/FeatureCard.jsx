ac022dc107d3046959e99ed7e822af9dbdd25bd3
Dillon Wilson <greasyRooster1@gmail.com>
2025-12-31 14:30:29 -0800

    not working

--- src/Pages/Landing/components/FeatureCard.jsx
+++ src/Pages/Landing/components/FeatureCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Card, Text, Heading, Box} from "@radix-ui/themes";
+import styles from "./featureCard.module.css";
 
 function FeatureCard(props) {
     return (
         <Box {...props} height="100%">
-            <Card style={{height:"100%"}}>
+            <Card className={styles.card}>
                 <Text size="5" weight="bold">{props.heading}</Text>
                 <br/>
                 <Text color="gray">{props.sub}</Text>
