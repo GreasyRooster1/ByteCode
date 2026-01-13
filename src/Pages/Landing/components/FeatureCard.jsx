ccb276b7754e40a4d4b198d6307ee765a45ae04b
GreasyRooster <greasyRooster1@gmail.com>
2026-01-12 17:02:04 -0800

    optional glow

--- src/Pages/Landing/components/FeatureCard.jsx
+++ src/Pages/Landing/components/FeatureCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Card, Text, Heading, Box} from "@radix-ui/themes";
+import {Box, Card, Text} from "@radix-ui/themes";
 import styles from "./featureCard.module.css";
 
 function FeatureCard(props) {
     return (
-        <Box {...props} className={`${styles.cardbox} ${props.className}`} height="100%">
+        <Box {...props} className={`${styles.cardbox} ${props.className} ${styles.glow}`} height="100%">
             <Card className={styles.card}>
                 <Text size="5" weight="bold">{props.heading}</Text>
                 <br/>
