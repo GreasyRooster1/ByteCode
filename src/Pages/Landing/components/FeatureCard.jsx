ca0df2597ba941e831810d805b2bfbaeac3776bf
GreasyRooster <greasyRooster1@gmail.com>
2026-01-15 18:07:36 -0800

    more card style

--- src/Pages/Landing/components/FeatureCard.jsx
+++ src/Pages/Landing/components/FeatureCard.jsx
@@ -4,13 +4,11 @@ import styles from "./featureCard.module.css";
 
 function FeatureCard(props) {
     return (
-        <Box {...props} className={`${styles.cardbox} ${props.className} ${styles.glow}`} height="100%">
-            <div className={styles.card}>
-                <Text size="5" weight="bold">{props.heading}</Text>
-                <br/>
-                <Text color="gray">{props.sub}</Text>
-                {props.children}
-            </div>
+        <Box {...props} className={`${styles.card} ${props.className} ${styles.glow}`} height="100%">
+            <Text size="5" weight="bold">{props.heading}</Text>
+            <br/>
+            <Text color="gray">{props.sub}</Text>
+            {props.children}
         </Box>
     );
 }
