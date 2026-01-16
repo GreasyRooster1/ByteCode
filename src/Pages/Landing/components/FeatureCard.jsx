520e82141cb2a4fc82fcd5b01f43f1202cf94260
GreasyRooster <greasyRooster1@gmail.com>
2026-01-15 18:19:37 -0800

    text gradient

--- src/Pages/Landing/components/FeatureCard.jsx
+++ src/Pages/Landing/components/FeatureCard.jsx
@@ -5,10 +5,13 @@ import styles from "./featureCard.module.css";
 function FeatureCard(props) {
     return (
         <Box {...props} className={`${styles.card} ${props.className} ${styles.glow}`} height="100%">
-            <Text size="5" weight="bold">{props.heading}</Text>
-            <br/>
-            <Text color="gray">{props.sub}</Text>
+            <div className={styles.text}>
+                <Text size="5" weight="bold">{props.heading}</Text>
+                <br/>
+                <Text color="gray">{props.sub}</Text><br/><br/><br/>
+            </div>
             {props.children}
+
         </Box>
     );
 }
