0e4be7c15e1882b9ba9c088ae110a4c3184aa080
GreasyRooster <greasyRooster1@gmail.com>
2026-01-15 17:49:35 -0800

    custom card style

--- src/Pages/Landing/components/FeatureCard.jsx
+++ src/Pages/Landing/components/FeatureCard.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Box, Card, Text} from "@radix-ui/themes";
+import {Box, Text} from "@radix-ui/themes";
 import styles from "./featureCard.module.css";
 
 function FeatureCard(props) {
     return (
         <Box {...props} className={`${styles.cardbox} ${props.className} ${styles.glow}`} height="100%">
-            <Card className={styles.card}>
+            <div className={styles.card}>
                 <Text size="5" weight="bold">{props.heading}</Text>
                 <br/>
                 <Text color="gray">{props.sub}</Text>
                 {props.children}
-            </Card>
+            </div>
         </Box>
     );
 }
