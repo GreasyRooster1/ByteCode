36f3861955cb3f669436e373a06882d5df61023e
Dillon Wilson <greasyRooster1@gmail.com>
2026-01-18 21:14:11 -0800

    animation

--- src/Pages/Landing/components/EditorGraphic.jsx
+++ src/Pages/Landing/components/EditorGraphic.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import {motion} from "motion/react";
 
 function EditorGraphic(props) {
+
+    const getLineAnim = (i,w,col)=>{
+        if(col){
+            return {
+                initial:{width:0,fill:col},
+                whileInView:{width:w,fill:col},
+                viewport:{ once: true, amount: 0.9 },
+                transition:{duration:1,delay:1+i/4},
+            }
+        }
+        return {
+            initial:{width:0},
+            whileInView:{width:w},
+            viewport:{ once: true, amount: 0.9 },
+            transition:{duration:1,delay:i/7},
+        }
+    }
+
     return (
         <motion.svg className={props.className} style={{width:"85S%"}} viewBox="0 0 533 305" fill="none" xmlns="http://www.w3.org/2000/svg">
             <mask id="mask0_26_2" style={{maskType:"alpha"}} maskUnits="userSpaceOnUse" x="0" y="0" width="533" height="305">
@@ -11,7 +29,7 @@ function EditorGraphic(props) {
                 initial={{height:0}}
                 whileInView={{height:305}}
                 viewport={{ once: true, amount: 0.9 }}
-                transition={{duration:2}
+                transition={{duration:1}}
 
                 id="codeMask" style={{maskType:"alpha"}} maskUnits="userSpaceOnUse" x="0" y="0" width="533" height="305">
                 <rect width="533" height="305" rx="9" fill="#0F0F10"/>
@@ -24,14 +42,18 @@ function EditorGraphic(props) {
                 <rect x="379" y="42" width="116" height="116" rx="17" fill="#DC1B4B"/>
             </g>
             <g mask="url(#codeMask">
-                <rect x="48" y="260" width="142" height="30" rx="15" fill="#F1C31C"/>
-                <rect x="14" y="187.046" width="207.2" height="29.6" rx="14.8" fill="#D019E8"/>
-                <rect x="48.1538" y="86.8615" width="214.031" height="29.6" rx="14.8" fill="#1CF16E"/>
+                <motion.rect {...getLineAnim(0,142)} x="14" y="14" height="30" rx="15" fill="#46494C"/>
+                <motion.rect {...getLineAnim(0,142,"#1974E8")} x="14" y="14" height="29.6" rx="14.8"/>
+                <motion.rect {...getLineAnim(1,214)} x="48.1538" y="50.4308" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(2,214,)} x="48.1538" y="86.8615" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(1,214,"#1CF16E")} x="48.1538" y="86.8615" height="29.6" rx="14.8"/>
+                <motion.rect {...getLineAnim(3,75)} x="14" y="123" height="30" rx="15" fill="#46494C"/>
 
-                <rect x="48.1538" y="223.477" width="243.631" height="29.6" rx="14.8" fill="#46494C"/>
-                <rect x="14" y="123" width="75" height="30" rx="15" fill="#46494C"/>
-                <rect x="14" y="14" width="214.031" height="29.6" rx="14.8" fill="#1974E8"/>
-                <rect x="48.1538" y="50.4308" width="214.031" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(3,207)} x="14" y="187.046" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(2,207,"#D019E8")} x="14" y="187.046" height="29.6" rx="14.8"/>
+                <motion.rect {...getLineAnim(5,243)} x="48.1538" y="223.477" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(6,142)}x="48" y="260" height="29.6" rx="14.8" fill="#46494C"/>
+                <motion.rect {...getLineAnim(3,142,"#F1C31C")} x="48" y="260" height="30" rx="15" />
             </g>
         </motion.svg>
 
