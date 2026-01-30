89e95f9c9671317f217e794a429607b18fd08721
GreasyRooster <greasyRooster1@gmail.com>
2026-01-29 18:17:31 -0800

    basic layout

--- src/Pages/OrgAdmin/OrgAdmin.jsx
+++ src/Pages/OrgAdmin/OrgAdmin.jsx
@@ -3,6 +3,9 @@ import {useParams} from "react-router-dom";
 import AuthLock from "~components/AuthLock.jsx";
 import ScreenPage from "~components/screenPage/ScreenPage.jsx";
 import {net} from "~api/net/net.js";
+import {Flex} from "@radix-ui/themes";
+
+import styles from "./orgAdmin.module.css";
 
 
 function OrgAdmin(props) {
@@ -13,7 +16,16 @@ function OrgAdmin(props) {
             return await net.org.isAdmin(auth.user?.access_token,[orgId],undefined)
         }}>
             <ScreenPage>
-                org admin
+
+                <Flex direction="row">
+                    <Flex p="4" height="100vh" className={styles.nav}>
+                        s
+                    </Flex>
+                    <Flex  className={styles.main}>
+
+                    </Flex>
+                </Flex>
+
             </ScreenPage>
         </AuthLock>
     );
