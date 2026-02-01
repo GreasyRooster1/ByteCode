2c999090a37f5acb5f18ea5d98b6a4660f9f358a
GreasyRooster <greasyRooster1@gmail.com>
2026-02-01 14:09:36 -0800

    selected

--- src/Pages/OrgAdmin/NavSection.jsx
+++ src/Pages/OrgAdmin/NavSection.jsx
@@ -3,8 +3,14 @@ import {Flex} from "@radix-ui/themes";
 import styles from "./navSection.module.css"
 
 function NavSection(props) {
+    const onclick = ()=>{
+        props.setSelectedTab(props.id);
+    }
+
+    let selected = props.id===props.selectedTab?styles.selected:"";
+
     return (
-        <Flex align="center" justify="start" px="2" py="1" m="1" className={styles.section}>
+        <Flex align="center" justify="start" px="2" py="1" m="1" className={`${styles.section} ${selected}`} onClick={onclick}>
             <span className={styles.iconWrapper}>
                 {props.icon}
             </span>
