5de954b33cdf7224d3626b00838385dc11c490c4
GreasyRooster <greasyRooster1@gmail.com>
2026-02-05 18:23:46 -0800

    award

--- src/Pages/OrgAdmin/InfoSection.jsx
+++ src/Pages/OrgAdmin/InfoSection.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Card, Container, Flex, Text} from "@radix-ui/themes";
+import UserIcon from "~icons/User.svg?react";
+import AwardIcon from "~icons/Award.svg?react";
 
 function InfoSection(props) {
     return (
@@ -7,6 +9,10 @@ function InfoSection(props) {
             <Card>
                 <Text size="8">Org Name</Text>
                 <Container><img src=""/></Container>
+                <Flex>
+                    <Text color="gray"><UserIcon/> 1038 Users</Text>
+                    <Text color="gray"><AwardIcon/> Pro</Text>
+                </Flex>
             </Card>
         </Flex>
     );
