8ffdaae1af8c9f3021772b152ba14e18c8381912
GreasyRooster <greasyRooster1@gmail.com>
2026-02-18 22:49:45 -0800

    logo

--- src/Pages/OrgAdmin/OrgAdmin.jsx
+++ src/Pages/OrgAdmin/OrgAdmin.jsx
@@ -3,7 +3,7 @@ import {useParams} from "react-router-dom";
 import AuthLock from "~components/AuthLock.jsx";
 import ScreenPage from "~components/screenPage/ScreenPage.jsx";
 import {net} from "~api/net/net.js";
-import {Flex, Separator} from "@radix-ui/themes";
+import {Flex, Separator, Text} from "@radix-ui/themes";
 import UserIcon from "../../icons/User.svg?react"
 
 
@@ -26,8 +26,15 @@ function OrgAdmin(props) {
 
                 <Flex direction="row">
                     <Flex direction="column" akl width="10%" height="100vh" className={styles.nav}>
-                        <NavSection icon={(<UserIcon />)} id={"info"} selectedTab={selectedTab} setSelectedTab={setSelectedTab}>Info</NavSection>
+                        <Flex align="center" justify="start" p="3">
+                            <img src="/logo.svg" alt="logo" width="48px"/>
+                            <Flex direction="column" ml="3">
+                                <Text size="2">ByteCode</Text>
+                                <Text size="1" color="gray">Org Admin</Text>
+                            </Flex>
+                        </Flex>
                         <Flex align="center" justify="center" width="100%"><Separator size="4" my="2" mx="2" /></Flex>
+                        <NavSection icon={(<UserIcon />)} id={"info"} selectedTab={selectedTab} setSelectedTab={setSelectedTab}>Info</NavSection>
                         <NavSection icon={(<UserIcon />)} id={"users"} selectedTab={selectedTab} setSelectedTab={setSelectedTab}>Users</NavSection>
                         <NavSection icon={(<UserIcon />)} id={"classes"} selectedTab={selectedTab} setSelectedTab={setSelectedTab}>Classes</NavSection>
                         <NavSection icon={(<UserIcon />)} id={"subscription"} selectedTab={selectedTab} setSelectedTab={setSelectedTab}>Subscription</NavSection>
