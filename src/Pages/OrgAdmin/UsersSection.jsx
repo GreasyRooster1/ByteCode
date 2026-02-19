397aba482f3e3cced1bd8144c27af66966be0236
GreasyRooster <greasyRooster1@gmail.com>
2026-02-19 15:10:35 -0800

    load users

--- src/Pages/OrgAdmin/UsersSection.jsx
+++ src/Pages/OrgAdmin/UsersSection.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Flex, Table} from "@radix-ui/themes";
+import {useAuth} from "react-oidc-context";
+import {net} from "~api/net/net.js";
+import {useReqState} from "~api/net/netutils.js";
 
 function UsersSection(props) {
+    let auth = useAuth();
+
+    const [requestState,setRequestState] = useReqState(true);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        net.proj.getUserProjDataList(auth.user?.access_token, [1] ,setRequestState).then(response => {
+            setUsers(response??[])
+        })
+    }, []);
+
     return (
         <Flex width="100%" height="100%" p="4" align="center" justify="center">
             <Table.Root>
@@ -14,23 +28,16 @@ function UsersSection(props) {
                 </Table.Header>
 
                 <Table.Body>
-                    <Table.Row>
-                        <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-                        <Table.Cell>danilo@example.com</Table.Cell>
-                        <Table.Cell>Developer</Table.Cell>
-                    </Table.Row>
+                    {
+                        users.map(user =>
+                            <Table.Row>
+                                <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+                                <Table.Cell>danilo@example.com</Table.Cell>
+                                <Table.Cell>Developer</Table.Cell>
+                            </Table.Row>
+                        )
+                    }
 
-                    <Table.Row>
-                        <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
-                        <Table.Cell>zahra@example.com</Table.Cell>
-                        <Table.Cell>Admin</Table.Cell>
-                    </Table.Row>
-
-                    <Table.Row>
-                        <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
-                        <Table.Cell>jasper@example.com</Table.Cell>
-                        <Table.Cell>Developer</Table.Cell>
-                    </Table.Row>
                 </Table.Body>
             </Table.Root>
         </Flex>
