c9f6c3b0daae5cc04f8cf2c1c2ba50940a1cbc96
GreasyRooster <greasyRooster1@gmail.com>
2026-02-19 15:15:20 -0800

    render data

--- src/Pages/OrgAdmin/UsersSection.jsx
+++ src/Pages/OrgAdmin/UsersSection.jsx
@@ -19,7 +19,7 @@ function UsersSection(props) {
 
     return (
         <Flex width="100%" height="100%" p="4" align="center" justify="center">
-            <Table.Root>
+            <Table.Root variant="surface">
                 <Table.Header>
                     <Table.Row>
                         <Table.ColumnHeaderCell>Username</Table.ColumnHeaderCell>
@@ -32,9 +32,9 @@ function UsersSection(props) {
                     {
                         users.map(user =>
                             <Table.Row>
-                                <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
-                                <Table.Cell>danilo@example.com</Table.Cell>
-                                <Table.Cell>Developer</Table.Cell>
+                                <Table.RowHeaderCell>{user.username}</Table.RowHeaderCell>
+                                <Table.Cell>{user.email}</Table.Cell>
+                                <Table.Cell>{user.role}</Table.Cell>
                             </Table.Row>
                         )
                     }
