17cdb1c62510a2911a6fb7d189b1d18f4cd4a736
GreasyRooster <greasyRooster1@gmail.com>
2026-02-19 18:05:12 -0800

    pending

--- src/Pages/OrgAdmin/UsersSection.jsx
+++ src/Pages/OrgAdmin/UsersSection.jsx
@@ -4,6 +4,7 @@ import {useAuth} from "react-oidc-context";
 import {net} from "~api/net/net.js";
 import {useReqState} from "~api/net/netutils.js";
 import UIToggle from "~components/UIToggle.jsx";
+import Pending from "~components/Pending.jsx";
 
 function UsersSection(props) {
     let auth = useAuth();
@@ -20,34 +21,36 @@ function UsersSection(props) {
 
     return (
         <Flex width="100%" height="100%" p="4" align="center" justify="center">
-            <Table.Root variant="surface">
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>Username</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell>Role</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
+            <Pending requestState={requestState}>
+                <Table.Root variant="surface">
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.ColumnHeaderCell>Username</Table.ColumnHeaderCell>
+                            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+                            <Table.ColumnHeaderCell>Role</Table.ColumnHeaderCell>
+                        </Table.Row>
+                    </Table.Header>
 
-                <Table.Body>
-                    <UIToggle value={users.length>0}>
-                        <UIToggle.True>
-                        {
-                            users.map(user =>
-                                <Table.Row>
-                                    <Table.RowHeaderCell>{user.username}</Table.RowHeaderCell>
-                                    <Table.Cell>{user.email}</Table.Cell>
-                                    <Table.Cell><Badge color={user.role==="Teacher"?"green":"gray"}>{user.role}</Badge></Table.Cell>
-                                </Table.Row>
-                            )
-                        }
-                        </UIToggle.True>
-                        <UIToggle.False>
-                            <Text>Your organization has no users yet!</Text>
-                        </UIToggle.False>
-                    </UIToggle>
-                </Table.Body>
-            </Table.Root>
+                    <Table.Body>
+                        <UIToggle value={users.length>0}>
+                            <UIToggle.True>
+                            {
+                                users.map(user =>
+                                    <Table.Row>
+                                        <Table.RowHeaderCell>{user.username}</Table.RowHeaderCell>
+                                        <Table.Cell>{user.email}</Table.Cell>
+                                        <Table.Cell><Badge color={user.role==="Teacher"?"green":"gray"}>{user.role}</Badge></Table.Cell>
+                                    </Table.Row>
+                                )
+                            }
+                            </UIToggle.True>
+                            <UIToggle.False>
+                                <Text>Your organization has no users yet!</Text>
+                            </UIToggle.False>
+                        </UIToggle>
+                    </Table.Body>
+                </Table.Root>
+            </Pending>
         </Flex>
     );
 }
