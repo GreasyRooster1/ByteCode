b64393cb8b623db69f8b96094e5382322998a228
GreasyRooster <greasyRooster1@gmail.com>
2026-02-19 18:04:07 -0800

    no users text

--- src/Pages/OrgAdmin/UsersSection.jsx
+++ src/Pages/OrgAdmin/UsersSection.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {Flex, Table} from "@radix-ui/themes";
+import {Badge, Flex, Table} from "@radix-ui/themes";
 import {useAuth} from "react-oidc-context";
 import {net} from "~api/net/net.js";
 import {useReqState} from "~api/net/netutils.js";
+import UIToggle from "~components/UIToggle.jsx";
 
 function UsersSection(props) {
     let auth = useAuth();
@@ -29,16 +30,22 @@ function UsersSection(props) {
                 </Table.Header>
 
                 <Table.Body>
-                    {
-                        users.map(user =>
-                            <Table.Row>
-                                <Table.RowHeaderCell>{user.username}</Table.RowHeaderCell>
-                                <Table.Cell>{user.email}</Table.Cell>
-                                <Table.Cell>{user.role}</Table.Cell>
-                            </Table.Row>
-                        )
-                    }
-
+                    <UIToggle value={users.length>0}>
+                        <UIToggle.True>
+                        {
+                            users.map(user =>
+                                <Table.Row>
+                                    <Table.RowHeaderCell>{user.username}</Table.RowHeaderCell>
+                                    <Table.Cell>{user.email}</Table.Cell>
+                                    <Table.Cell><Badge color={user.role==="Teacher"?"green":"gray"}>{user.role}</Badge></Table.Cell>
+                                </Table.Row>
+                            )
+                        }
+                        </UIToggle.True>
+                        <UIToggle.False>
+                            <Text>Your organization has no users yet!</Text>
+                        </UIToggle.False>
+                    </UIToggle>
                 </Table.Body>
             </Table.Root>
         </Flex>
